9c1cddfe8f44998f5ad84cf28cce8af2b6dbd367
Nikhilesh Shah <nikhileshshah10@gmail.com>
2022-07-09 12:57:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from translate import Translator
+import streamlit as st
+st.set_page_config(page_title='Nik Translator', page_icon=":earth_asia:")
+st.title("Text Translator ❤")
+st.subheader("Type your text here ")
+text=st.text_input("Write here")
+lan1=st.selectbox("Select Language from",["English","Hindi","Spanish","French","German","Japanese"])
+lan2=st.selectbox("Translate to ",["English","Hindi","Spanish","French","German","Japanese"])
+
+translator=  Translator(from_lang=lan1.lower(),to_lang=lan2.lower())
+translation = translator.translate(text)
+page_bg_img = '''
+ <style>
+    .stApp {
+    background-image: url("https://images-na.ssl-images-amazon.com/images/I/61+oIVFF7FL.png");
+    background-size: cover;
+    }
+    </style>
+    '''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+if(st.button("TRANSLATE")): 
+    st.header(' ')
+    st.header(translation)
