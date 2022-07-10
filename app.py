ee56daedc7b0fd2f8a3be3e57cece563159b4918
Nikhilesh Shah <nikhileshshah10@gmail.com>
2022-07-10 15:43:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from translate import Translator
+from googletrans import Translator
 import streamlit as st
 st.set_page_config(page_title='Nik Translator', page_icon=":earth_asia:")
 st.title("Text Translator ❤")
 st.subheader("Type your text here ")
-text=st.text_input("Write here")
+sent=st.text_input("Write here")
 lan1=st.selectbox("Select Language from",["English","Hindi","Spanish","French","German","Japanese"])
-lan2=st.selectbox("Translate to ",["Spanish","English","German","French","Hindi","Japanese"])
+lan2=st.selectbox("Translate to ",["English","Hindi","Spanish","French","German","Japanese"])
 
-translator=  Translator(from_lang=lan1.lower(),to_lang=lan2.lower())
-translation = translator.translate(text)
+translator=  Translator()
+translation = translator.translate(sent,dest=lan2.lower()[0:2],src=lan1.lower()[0:2])
 page_bg_img = '''
  <style>
-    .stApp,.e8zbici2 {
+    .stApp {
     background-image: url("https://images-na.ssl-images-amazon.com/images/I/61+oIVFF7FL.png");
     background-size: cover;
     }
@@ -22,4 +22,4 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 if(st.button("TRANSLATE")): 
     st.header(' ')
-    st.header(translation)
+    st.header(translation.text)
